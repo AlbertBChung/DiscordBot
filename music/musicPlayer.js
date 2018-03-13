6c60b08952e2909ed702152aa899031fd8a4790e
Albert Chung <albertc435@gmail.com>
2018-03-13 14:39:28 -0400

    fix bugs

--- music/musicPlayer.js
+++ music/musicPlayer.js
@@ -12,11 +12,12 @@ var dispatcher = [];
 
 
 
+
+/**
 Music.remove({ }, function (err) {
   if (err) return handleError(err);
   console.log('d all')
 });
-/**
   STEPS:
   1. fetch youtube videos
   2. send embeds to channels
@@ -42,10 +43,13 @@ var getPlayer = function(serverId, callback){
       musicPlayer.save(function(err) {
         if (err) throw err;
         console.log('New music player', musicPlayer);
+        callback(musicPlayer);
       });
+    } else {
+      callback(musicPlayer)
     }
 
-    return callback(musicPlayer);
+
   });
 }
 
@@ -60,8 +64,8 @@ exports.music = function music(args, message){
         }
         if(musicPlayer.queue[0]){
           if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
-            play(connection, musicPlayer);
-          }).catch(console.log)
+            play(connection, musicPlayer, message.channel);
+          }).catch(console.log('play'))
         } else {
           message.channel.send('Queue is empty! Add a song');
         }
@@ -128,7 +132,7 @@ var sendList = function(vidsList, discord_message, musicPlayer){
 
 var getVids = function(searchString){
   return new Promise(function(resolve, reject){
-    youTube.search(searchString, numOfOptions, function(error, result) {
+    youTube.search(searchString, numOfOptions, {'type': 'video'}, function(error, result) {
       if (error) {
         reject(error);
       }
@@ -184,7 +188,7 @@ exports.handleSelect = function(message, music){
 }
 
 
-var play = function(connection, musicPlayer){
+var play = function(connection, musicPlayer, channel){
   console.log(musicPlayer.queue[0])
   dispatcher[musicPlayer.serverId] = connection.playStream(YTDL(musicPlayer.queue[0], {filter: 'audioonly'}));
   musicPlayer.queue.shift();
@@ -192,8 +196,16 @@ var play = function(connection, musicPlayer){
     if (err) throw err;
   });
   dispatcher[musicPlayer.serverId].on('end', function(){
+    var rand = Math.floor((Math.random() * 10) + 1);
+    if(rand == 1){
+      musicPlayer.queue.unshift('https://www.youtube.com/watch?v=2cjbSgy3vSw');
+      var embed = new Discord.RichEmbed()
+        .addField('AHHHHHHHHHHHHHHHHHHHHHHHHH', 'GO FURTHER BEYONDDD', true)
+        .setThumbnail('https://i.ytimg.com/vi/2cjbSgy3vSw/hqdefault.jpg')
+      channel.send('decided to disobey and play whatever I want',embed);
+    }
     if(musicPlayer.queue[0]){
-      play(connection, musicPlayer);
+      play(connection, musicPlayer, channel);
     } else {
       connection.disconnect();
     }
